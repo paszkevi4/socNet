a16998de57c8d5d8059fffaf249b13d1060ed302
paszkevi4 <paszkevi4@aol.com>
2020-05-26 14:24:54 +0300

    Updates

---
+++ precache-manifest.9c7acd89abe7b71ab19eb0f42bd2b747.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55162f1b802e65d945a02c9defab1f08",
+    "url": "/socNet/index.html"
+  },
+  {
+    "revision": "23fa3de1be172067cb91",
+    "url": "/socNet/static/css/main.9a61d29f.chunk.css"
+  },
+  {
+    "revision": "dd01f87c2819ac7ad416",
+    "url": "/socNet/static/js/2.22cb43a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socNet/static/js/2.22cb43a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fa3de1be172067cb91",
+    "url": "/socNet/static/js/main.fb0d886e.chunk.js"
+  },
+  {
+    "revision": "76f07ab24c8d750816eb",
+    "url": "/socNet/static/js/runtime-main.d910f66a.js"
+  },
+  {
+    "revision": "f8b61afeaa85a6f37235dd5a2d2e2b6c",
+    "url": "/socNet/static/media/cover.f8b61afe.png"
+  },
+  {
+    "revision": "adab92f3e5f7ece64ea50c01cd7969f3",
+    "url": "/socNet/static/media/defaultAvatar.adab92f3.svg"
+  },
+  {
+    "revision": "315b2175a8933f2dc1e5eaae28097b3c",
+    "url": "/socNet/static/media/fetching.315b2175.svg"
+  },
+  {
+    "revision": "c24eb07f6b509a287322b065546e46c2",
+    "url": "/socNet/static/media/loginCover.c24eb07f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/socNet/static/media/logo.5d5d9eef.svg"
+  }
+]);
